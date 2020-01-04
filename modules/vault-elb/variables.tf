66160d4f0ff668941e9cdf4b2524045aa1a4180e
George Lerma <glerma@ftsc.com>
2020-01-03 21:05:12 -0600

    removed unneeded environment variable

--- modules/vault-elb/variables.tf
+++ modules/vault-elb/variables.tf
@@ -138,5 +138,3 @@ variable "region" {
   default     = "us-east-1"
 }
 
-variable "environment" {
-}
--- modules/vault-security-group-rules/variables.tf
+++ modules/vault-security-group-rules/variables.tf
@@ -47,5 +47,3 @@ variable "region" {
   default     = "us-east-1"
 }
 
-variable "environment" {
-}
