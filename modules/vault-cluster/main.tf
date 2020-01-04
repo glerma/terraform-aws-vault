2c7341484e91b63f42a1eadca84c6c824a3a8188
George Lerma <glerma@ftsc.com>
2020-01-03 20:57:51 -0600

    added missing member_account parameter

--- modules/vault-cluster/main.tf
+++ modules/vault-cluster/main.tf
@@ -222,7 +222,7 @@ resource "aws_security_group_rule" "allow_all_outbound" {
 
 module "security_group_rules" {
   source = "../vault-security-group-rules"
-
+  member_account = var.member_account
   security_group_id                    = aws_security_group.lc_security_group.id
   allowed_inbound_cidr_blocks          = var.allowed_inbound_cidr_blocks
   allowed_inbound_security_group_ids   = var.allowed_inbound_security_group_ids
